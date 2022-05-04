40712fa1f4cc0a490d33950327e1b08b13bdf8f0
saidugurlu <ugurlumsaid@gmx.de>
2022-05-04 13:04:37 +0200

    displaying employees async

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,31 @@
-import "./App.css";
-import employees from './data/employees.json';
+import { useEffect, useState } from 'react';
+import './App.css';
+const employeesUrl =
+	'https://raw.githubusercontent.com/graphql-compose/graphql-compose-examples/master/examples/northwind/data/json/employees.json';
+
 function App() {
-  return <div className="App">
-    
-<h1>Employess</h1>
+	const [employees, setEmployees] = useState([]);
 
-
-{employees.map ((emp, i)=>{
-  return (
-<div key={i} className="employee">
-{emp.firstName} {emp.lastName}
-
-
-
-</div>
-
-
-
-  )
-})}
-
-  </div>;
+	useEffect(() => {
+		(async () => {
+			const response = await fetch(employeesUrl);
+			const _employees = await response.json();
+			setEmployees(_employees);
+		})();
+	}, []);
+	return (
+		<div className="App">
+			<h1>Employees</h1>
+			<div className="employees">
+				{employees.map((emp, index) => {
+					return (
+						<div key={index} className="employee">
+							{`${emp.firstName} ${emp.lastName}`}
+						</div>
+					);
+				})}
+			</div>
+		</div>
+	);
 }
-
 export default App;
