13c1b45f88b5385f30c672817a53d42312e36b1f
saidugurlu <ugurlumsaid@gmx.de>
2022-05-04 11:22:03 +0200

    local data creted, imported and shown

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,26 @@
 import "./App.css";
-
+import employees from './data/employees.json';
 function App() {
-  return <div className="App">Hallo</div>;
+  return <div className="App">
+    
+<h1>Employess</h1>
+
+
+{employees.map ((emp, i)=>{
+  return (
+<div key={i} className="employee">
+{emp.firstName} {emp.lastName}
+
+
+
+</div>
+
+
+
+  )
+})}
+
+  </div>;
 }
 
 export default App;
